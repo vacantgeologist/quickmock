0f6c9f3c8653897362b7c79d86407ee6a0c98c4c
vacantgeologist <jwcubnxxit@protonmail.com>
2018-03-03 09:02:20 +0900

    Mock set up

---
+++ components/Mock.vue
@@ -0,0 +1,8 @@
+<template>
+<h1>Mock here</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,64 +1,63 @@
 <template>
   <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        quickmock
-      </h1>
-      <h2 class="subtitle">
-        An easy way to create dynamic mocks with Vue
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+    <div class="phone">
+      <img class="phone__image" src="/iphone_mock.png">
+      <div class="phone__mockArea">
+        <mock></mock>
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Mock from '~/components/Mock.vue'
 
 export default {
   components: {
-    AppLogo
+    Mock 
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+$color__bg1: rgba(26, 97, 138, 0.5);
+$color__bg2: rgba(94, 197, 175, 0.5);
+$dropshadow: #333;
+
+$phone_width: 320px;
+$phone_height: 640px;
+$screen_width: 304px;
+$screen_height: 506px;
+
 .container {
+  background-image: linear-gradient(-0.05turn, $color__bg1 0%, $color__bg2 100%);
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
 }
+.phone {
+  &__image {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-left: $phone_width / 2 * -1;
+    margin-top: $phone_height / 2 * -1 + 6;
+    width: $phone_width;
+    height: $phone_height;
+    filter: drop-shadow(20px 20px 30px $dropshadow);
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  }
+  &__mockArea {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    margin-left: $screen_width / 2 * -1;
+    margin-top: $screen_height / 2 * -1;
+    width: $screen_width;
+    height: $screen_height;
+    background-color: #eee;
+  }
 }
 </style>
